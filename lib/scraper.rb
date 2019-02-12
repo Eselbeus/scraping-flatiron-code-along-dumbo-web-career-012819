d54f8f40646269a0c560efe690125d45167828d0
Seann Branchfield <eselbeus@gmail.com>
2019-02-12 02:26:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,11 +16,10 @@ class Scraper
       #   course.schedule = post.css(".date").text
       #   course.description = post.css("p").text
       end
-      binding.pry
     end
     
     def get_courses 
-      
+      self.get_page.css(".post")
     end
   
     def print_courses
